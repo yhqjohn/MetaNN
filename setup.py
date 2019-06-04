be8b1badb06782be0384d14b3ccce9b0e73ee209
余翰乔 <yhqjohn@gmail.com>
2019-06-04 13:55:35 +0800

    upgrade to 0.1.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ readme = open('README.rst').read()
 
 setup(
     name='MetaNN',
-    version='0.1.1',
+    version='0.1.2',
     description='MetaNN provides extensions of PyTorch nn.Module for meta learning',
     author='Zhi Zhang',
     author_email='yhqjohn@gmail.com',
